23f56e000d4ee5fa60a0fa0ae0a6f0c5c7061f77
Narender Singh <narender2031@gmail.com>
2022-02-02 21:51:44 +0530

    inital commit

---
+++ src/lib/Env.js
@@ -0,0 +1,2 @@
+export const MY_API_KEY = import.meta.env.VITE_MY_API_KEY;
+export const MY_URL = import.meta.env.VITE_MY_URL;
---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { MY_API_KEY } from '$lib/Env';
+    // console.log(MY_API_KEY);
+
+    let myApiKey;
+
+    if (process.env.NODE_ENV === 'production') {
+        // For production
+        myApiKey = process.env.MY_API_KEY;
+    } else {
+        // For development
+        myApiKey = MY_API_KEY;
+    }
+
+    console.log(myApiKey);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
